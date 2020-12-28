d4520dcafcd1f91d468143acec5e0821aa5033ba
Jens E. Pedersen <jensegholm@protonmail.com>
2020-12-28 10:12:46 +0100

    Solved publishing for anaconda with org

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM continuumio/miniconda3:4.9.2
 LABEL "repository"="https://github.com/jegp/conda-package-publish-action"
 LABEL "maintainer"="Jens E. Pedersen <jens@jepedersen.dk>"
 
+RUN apt-get update && apt-get dist-upgrade -y
+RUN apt-get install -y libgl1-mesa-dev && rm -rf /var/lib/apt/lists/*
 RUN conda install -y anaconda-client conda-build conda-verify gxx_linux-64
 
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,16 @@
 set -ex
 set -o pipefail
 
-shopt -s globstar
-
 go_to_build_dir() {
     if [ ! -z $INPUT_SUBDIR ]; then
         cd $INPUT_SUBDIR
+    fi
+}
+
+check_if_setup_file_exists() {
+    if [ ! -f setup.py ]; then
+        echo "setup.py must exist in the directory that is being packaged and published."
+        exit 1
     fi
 }
 
@@ -19,16 +24,32 @@ check_if_meta_yaml_file_exists() {
 }
 
 build_package(){
+    # Build for Linux
     conda build -c conda-forge -c pytorch -c bioconda --output-folder . .
-    conda convert -p osx-64 /opt/conda/conda-bld/linux-64/*.tar.bz2
+
+    # Convert to other platforms: OSX, WIN
+    if [[ $INPUT_PLATFORMS == *"osx"* ]]; then
+    conda convert -p osx-64 linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"win"* ]]; then
+    conda convert -p win-64 linux-64/*.tar.bz2
+    fi
 }
 
 upload_package(){
     export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
-    anaconda upload --label main /opt/conda/conda-bld/linux-64/*.tar.bz2
-    anaconda upload --label main osx-64/*.tar.bz2
+    if [[ $INPUT_PLATFORMS == *"osx"* ]]; then
+    anaconda upload --label main $INPUT_PUBLISHFLAGS osx-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"linux"* ]]; then
+    anaconda upload --label main -u $INPUT_PUBLISHFLAGS linux-64/*.tar.bz2
+    fi
+    if [[ $INPUT_PLATFORMS == *"win"* ]]; then
+    anaconda upload --label main -u $INPUT_PUBLISHFLAGS win-64/*.tar.bz2
+    fi
 }
 
+check_if_setup_file_exists
 go_to_build_dir
 check_if_meta_yaml_file_exists
 build_package
